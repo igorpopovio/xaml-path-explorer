8633838060a888da8fd54e4239874183a0b20a41
Igor Popov <igor@igorpopov.io>
2017-08-05 14:51:11 +0300

    Improve drag and drop
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- XamlPathExplorer/MainWindow.xaml.cs
+++ XamlPathExplorer/MainWindow.xaml.cs
@@ -11,16 +11,16 @@ namespace XamlPathExplorer {
             InitializeComponent();
         }
 
-        private void wrapPanel_DragEnter(object sender, DragEventArgs e) {
-            wrapPanel.Style = (Style)FindResource("DragEnter");
+        public void wrapPanel_DragEnter(object sender, DragEventArgs e) {
+            itemsPanel.Style = (Style)FindResource("DragEnter");
         }
 
-        private void wrapPanel_DragLeave(object sender, DragEventArgs e) {
-            wrapPanel.Style = (Style)FindResource("DragLeave");
+        public void wrapPanel_DragLeave(object sender, DragEventArgs e) {
+            itemsPanel.Style = (Style)FindResource("DragLeave");
         }
 
-        private void wrapPanel_Drop(object sender, DragEventArgs e) {
-            wrapPanel.Style = (Style)FindResource("DragLeave");
+        public void wrapPanel_Drop(object sender, DragEventArgs e) {
+            itemsPanel.Style = (Style)FindResource("DragLeave");
 
             if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
                 var files = (string[])e.Data.GetData(DataFormats.FileDrop);
@@ -28,29 +28,28 @@ namespace XamlPathExplorer {
             }
         }
 
-        private void LoadGeometryInBackgroundThreadFrom(string[] files) {
+        public void LoadGeometryInBackgroundThreadFrom(string[] files) {
             var worker = new GeometryBackgroundWorker();
             worker.ProgressChanged += Worker_ProgressChanged;
             worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
 
-            wrapPanel.Children.Clear();
+            itemsPanel.Children.Clear();
             worker.RunWorkerAsync(files);
             statusBarItem.Content = $"Starting to load paths from {files}";
         }
 
-        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+        public void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
             var count = e.Result as int?;
             statusBarItem.Content = $"Loaded {count} paths";
         }
 
-        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+        public void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
             var pathGeometry = e.UserState as string;
             LoadGeometry(pathGeometry);
         }
 
-        private void LoadGeometry(string geometry) {
-            wrapPanel.Children.Add(new PathButton { PathGeometry = Geometry.Parse(geometry) });
+        public void LoadGeometry(string geometry) {
+            itemsPanel.Children.Add(new PathButton { PathGeometry = Geometry.Parse(geometry) });
         }
-
     }
 }
