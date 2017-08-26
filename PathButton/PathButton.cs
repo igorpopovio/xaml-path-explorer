968360ff141d611f1362788472266460dba758b1
Igor Popov <igor@igorpopov.io>
2017-08-26 08:50:23 +0300

    Add path button custom control
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ PathButton/PathButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PathButton {
+    public class PathButton : Button
+    {
+        static PathButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PathButton),
+                new FrameworkPropertyMetadata(typeof(PathButton)));
+        }
+    }
+}
